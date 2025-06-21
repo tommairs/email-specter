fea511f38266258344b6d059498b9f89d958c9a9
maileroo <service@maileroo.com>
2025-06-21 22:17:08 +1000

    Webhook

--- model/message.go
+++ model/message.go
@@ -3,6 +3,7 @@ package model
 import (
 	"context"
 	"email-specter/database"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"time"
 )
@@ -31,7 +32,36 @@ type Message struct {
 	UpdatedAt                        time.Time          `json:"updated_at" bson:"updated_at"`
 }
 
+func GetMessageByKumoMtaID(kumoMtaID string) (*Message, error) {
+
+	collection := database.MongoConn.Collection("messages")
+
+	var message Message
+	err := collection.FindOne(context.Background(), bson.M{"kumo_mta_id": kumoMtaID}).Decode(&message)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &message, nil
+
+}
+
 func (m *Message) Save() error {
+
+	collection := database.MongoConn.Collection("messages")
+
+	_, err := collection.UpdateOne(context.Background(), bson.M{"_id": m.ID}, bson.M{"$set": m})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func (m *Message) Insert() error {
 
 	collection := database.MongoConn.Collection("messages")
 
--- web/webhook/service.go
+++ web/webhook/service.go
@@ -4,7 +4,9 @@ import (
 	"crypto/subtle"
 	"email-specter/model"
 	"encoding/json"
+	"errors"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"net"
 	"os"
 	"regexp"
@@ -55,7 +57,7 @@ func isAuthenticated(id string, token string) bool {
 	}
 
 	if subtle.ConstantTimeCompare([]byte(mta.SecretToken), []byte(token)) == 0 {
-		return true
+		return false
 	}
 
 	return true
@@ -134,11 +136,11 @@ func getDomain(email string) string {
 
 }
 
-func getServiceName(mxRecord string, domain string) string {
+func getServiceName(peerName string, domain string) string {
 
 	for _, provider := range ServiceProviders {
 
-		if provider.CompiledRegex.MatchString(mxRecord) || provider.CompiledRegex.MatchString(domain) {
+		if provider.CompiledRegex.MatchString(peerName) || provider.CompiledRegex.MatchString(domain) {
 			return provider.Name
 		}
 
@@ -148,7 +150,63 @@ func getServiceName(mxRecord string, domain string) string {
 
 }
 
+func handleBounceEvent(mtaId primitive.ObjectID, webhookData model.WebhookEvent) bool {
+
+	currentTime := time.Now()
+
+	event := model.Event{
+		Type:     webhookData.Type,
+		Content:  webhookData.Response.Content,
+		Datetime: currentTime,
+	}
+
+	message, err := getOrCreateMessage(mtaId, webhookData, currentTime)
+
+	if err != nil {
+		return false
+	}
+
+	return updateMessageStatus(message, event, webhookData.Type, currentTime)
+
+}
+
+func handleTransientFailureEvent(mtaId primitive.ObjectID, webhookData model.WebhookEvent) bool {
+
+	currentTime := time.Now()
+
+	event := model.Event{
+		Type:     webhookData.Type,
+		Content:  webhookData.Response.Content,
+		Datetime: currentTime,
+	}
+
+	message, err := getOrCreateMessage(mtaId, webhookData, currentTime)
+
+	if err != nil {
+		return false
+	}
+
+	return updateMessageStatus(message, event, webhookData.Type, currentTime)
+
+}
+
 func handleDeliveryEvent(mtaId primitive.ObjectID, webhookData model.WebhookEvent) bool {
+
+	currentTime := time.Now()
+
+	event := model.Event{
+		Type:     webhookData.Type,
+		Content:  webhookData.Response.Content,
+		Datetime: currentTime,
+	}
+
+	message, err := getOrCreateMessage(mtaId, webhookData, currentTime)
+
+	if err != nil {
+		return false
+	}
+
+	return updateMessageStatus(message, event, webhookData.Type, currentTime)
 
 }
 
@@ -156,17 +214,33 @@ func handleReceptionEvent(mtaId primitive.ObjectID, webhookData model.WebhookEve
 
 	currentTime := time.Now()
 
-	sourceIpAddress := getIPAddress(webhookData.SourceAddress.Address)
-	sourceDomain := getDomain(webhookData.Sender)
-	receiverDomain := getDomain(webhookData.Recipient)
-
 	events := []model.Event{
 		{
-			Type:     "Accepted",
+			Type:     webhookData.Type,
 			Content:  "",
 			Datetime: currentTime,
 		},
 	}
+
+	message := createMessage(mtaId, currentTime, webhookData)
+
+	message.Events = events
+
+	err := message.Insert()
+
+	if err != nil {
+		return false
+	}
+
+	return true
+
+}
+
+func createMessage(mtaId primitive.ObjectID, currentTime time.Time, webhookData model.WebhookEvent) *model.Message {
+
+	sourceIpAddress := getIPAddress(webhookData.SourceAddress.Address)
+	sourceDomain := getDomain(webhookData.Sender)
+	receiverDomain := getDomain(webhookData.Recipient)
 
 	message := model.Message{
 		ID:                               primitive.NewObjectID(),
@@ -178,20 +252,51 @@ func handleReceptionEvent(mtaId primitive.ObjectID, webhookData model.WebhookEve
 		DestinationDomain:                receiverDomain,
 		Sender:                           webhookData.Sender,
 		Recipient:                        webhookData.Recipient,
-		Events:                           events,
+		Events:                           []model.Event{},
 		KumoMtaBounceClassification:      "",
 		EmailSpecterBounceClassification: "",
-		LastStatus:                       "Accepted",
+		LastStatus:                       webhookData.Type,
 		CreatedAt:                        currentTime,
 		UpdatedAt:                        currentTime,
 	}
 
-	err := message.Save()
+	return &message
+
+}
+
+func updateMessageStatus(message *model.Message, event model.Event, status string, currentTime time.Time) bool {
+
+	message.Events = append(message.Events, event)
+
+	message.LastStatus = status
+	message.UpdatedAt = currentTime
+
+	return message.Save() == nil
+
+}
+
+func getOrCreateMessage(mtaId primitive.ObjectID, webhookData model.WebhookEvent, currentTime time.Time) (*model.Message, error) {
+
+	message, err := model.GetMessageByKumoMtaID(webhookData.ID)
 
 	if err != nil {
-		return false
+
+		if errors.Is(err, mongo.ErrNoDocuments) {
+
+			message = createMessage(mtaId, currentTime, webhookData)
+
+			if err := message.Save(); err != nil {
+				return nil, err
+			}
+
+			return message, nil
+
+		}
+
+		return nil, err
+
 	}
 
-	return true
+	return message, nil
 
 }
